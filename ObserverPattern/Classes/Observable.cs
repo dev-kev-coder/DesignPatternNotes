29d244b632669e817820b7b770b2c64205d7c4e9
Kevin Albarran <recodedkevdev@gmail.com>
2024-01-03 23:12:40 -0800

    created class for handling Obserable functionlaity

---
+++ ObserverPattern/Classes/Observable.cs
@@ -0,0 +1,58 @@
+﻿using ObserverPattern.Interfaces.Subject;
+using ObserverPattern.Interfaces.Observer;
+using System;
+
+namespace ObserverPattern.Classes
+{
+    public class Observable : ISubject
+    {
+        private readonly List<IObserver> _observers = new();
+
+        private bool _changed;
+
+        public string RegisterObserver(IObserver observer)
+        {
+            var indexOfObserver = _observers.IndexOf(observer);
+
+            if (indexOfObserver != -1) return "Observer has alrady been added";
+
+            _observers.Add(observer);
+
+            return "Observer added successfully";
+        }
+
+        public string RemoveObserver(IObserver observer)
+        {
+            var indexOfObserver = _observers.IndexOf(observer);
+
+            if (indexOfObserver == -1) return "Observer Does not exist";
+
+            _observers.RemoveAt(indexOfObserver);
+
+            return "Observer removed successfully";
+        }
+
+        public void NotifyObservers()
+        {
+            _observers.ForEach(observer =>
+            {
+                if (_changed)
+                {
+                    observer.Update(this);
+                }
+            });
+
+            ClearChanged();
+        }
+
+        public void SetChanged()
+        {
+            _changed = true;
+        }
+
+        public void ClearChanged()
+        {
+            _changed = false;
+        }
+    }
+}
